d604282718d66374f386595ca6d4c521185ac233
Carbon225 <carbon225@protonmail.com>
2022-03-13 13:15:05 +0100

    [c01] all benches

--- cw01/KarbowskiJakub/cw01/zad2/src/bench.c
+++ cw01/KarbowskiJakub/cw01/zad2/src/bench.c
@@ -5,8 +5,11 @@
 #include "zad1.h"
 #include "time_utils.h"
 
-#define BENCH_COUNT (64)
-#define MAX_FILE_COUNT (32)
+#define BENCH1_COUNT (1 << 9)
+#define BENCH2_COUNT (1 << 15)
+#define BENCH4_COUNT (1 << 12)
+#define MAX_FILE_COUNT (1 << 8)
+#define MAX_BLOCKS (1 << 15)
 
 int benchmarks_run(int num_files, char **in_files)
 {
@@ -15,35 +18,121 @@ int benchmarks_run(int num_files, char **in_files)
     int err = 0;
     bench_t bench;
     barr_t *barr;
-    int created_fds[MAX_FILE_COUNT * BENCH_COUNT];
+    int created_fds[MAX_FILE_COUNT];
     int created_files = 0;
+    size_t created_block_idx[MAX_BLOCKS];
+    int created_blocks = 0;
 
     do
     {
-        barr = barr_alloc(MAX_FILE_COUNT * BENCH_COUNT);
+        barr = barr_alloc(MAX_BLOCKS + 1);
         if (!barr) break;
+
+        // BENCH WC_FILES
 
         bench_start(&bench);
 
-        for (int count = 0; count < BENCH_COUNT; ++count)
+        for (int count = 0; count < BENCH1_COUNT; ++count)
         {
-            for (int i = 0; i < num_files; ++i)
+            int fd = generate_stats_file(in_files[count % num_files]);
+            if (fd < 0)
             {
-                int fd = generate_stats_file(in_files[i]);
-                if (fd < 0)
-                {
-                    err = -1;
-                    break;
-                }
+                err = -1;
+                break;
+            }
+            if (created_files < MAX_FILE_COUNT)
+            {
                 created_fds[created_files] = fd;
                 created_files++;
             }
-            if (err) break;
+            else
+            {
+                close(fd);
+            }
         }
         if (err) break;
 
         bench_stop(&bench);
-        printf("[BENCH] wc files:\n");
+        printf("[BENCH] WC FILES:\n");
+        bench_print(&bench);
+
+        // BENCH LOAD BLOCKS
+
+        bench_start(&bench);
+
+        for (int count = 0; count < BENCH2_COUNT; ++count)
+        {
+            size_t b_index;
+            err = barr_block_load(barr, created_fds[count % created_files], &b_index);
+            if (err) break;
+            if (created_blocks < MAX_BLOCKS)
+            {
+                created_block_idx[created_blocks] = b_index;
+                created_blocks++;
+            }
+            else
+            {
+                err = barr_block_delete(barr, b_index);
+                if (err) break;
+            }
+        }
+        if (err) break;
+
+        bench_stop(&bench);
+        printf("[BENCH] LOAD BLOCKS:\n");
+        bench_print(&bench);
+
+        // BENCH DELETE BLOCKS
+
+        bench_start(&bench);
+
+        for (int i = 0; i < created_blocks; ++i)
+        {
+            err = barr_block_delete(barr, created_block_idx[i]);
+            if (err) break;
+        }
+        if (err) break;
+        created_blocks = 0;
+
+        bench_stop(&bench);
+        printf("[BENCH] DELETE BLOCKS:\n");
+        bench_print(&bench);
+
+        // BENCH LOAD DELETE
+
+        bench_start(&bench);
+
+        for (int count = 0; count < BENCH4_COUNT; ++count)
+        {
+            for (int i = 0; i < created_files; ++i)
+            {
+                size_t index;
+                err = barr_block_load(barr, created_fds[i], &index);
+                if (err) break;
+                if (created_blocks < MAX_BLOCKS)
+                {
+                    created_block_idx[created_blocks] = index;
+                    created_blocks++;
+                }
+                else
+                {
+                    err = barr_block_delete(barr, index);
+                    if (err) break;
+                }
+            }
+            if (err) break;
+            for (int i = 0; i < created_blocks; ++i)
+            {
+                err = barr_block_delete(barr, created_block_idx[i]);
+                if (err) break;
+            }
+            if (err) break;
+            created_blocks = 0;
+        }
+        if (err) break;
+
+        bench_stop(&bench);
+        printf("[BENCH] LOAD DELETE:\n");
         bench_print(&bench);
     } while (0);
 
@@ -54,5 +143,5 @@ int benchmarks_run(int num_files, char **in_files)
 
     if (barr) barr_free(barr);
 
-    return 0;
+    return err;
 }
