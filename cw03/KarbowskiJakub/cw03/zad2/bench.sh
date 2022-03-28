6cda008b179923fe798e0b6a64eddfdfb00d240a
Carbon225 <carbon225@protonmail.com>
2022-03-28 19:05:10 +0200

    [c03] better bench

--- cw03/KarbowskiJakub/cw03/zad2/bench.sh
+++ cw03/KarbowskiJakub/cw03/zad2/bench.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-for n in $(seq 1 12)
+for n in $(seq 1 30)
 do
   echo "n = $n"
-  (time -p "$1" 0.000000001 "$n" > /dev/null) 2>&1
+  (time -p "$1" 0.0000000005 "$n" > /dev/null) 2>&1
 done
 
 echo ''
 
-for dx in 0.0000001 0.00000001 0.000000001
+for dx in 0.0000001 0.00000001 0.000000001 0.0000000005
 do
   echo "dx = $dx"
   (time -p "$1" "$dx" 1 > /dev/null) 2>&1
