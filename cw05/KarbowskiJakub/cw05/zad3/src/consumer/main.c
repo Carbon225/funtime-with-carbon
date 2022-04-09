762922de0e1ef670c0537feee72a3a54a8a1d7e4
Carbon225 <carbon225@protonmail.com>
2022-04-09 19:44:29 +0200

    [c05] consumer help

--- cw05/KarbowskiJakub/cw05/zad3/src/consumer/main.c
+++ cw05/KarbowskiJakub/cw05/zad3/src/consumer/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+static const char HELP[] =
+        "SO Lab5 Zad3 - Jakub Karbowski\n"
+        "Usage:\n"
+        "%s FIFO OUTPUT N\n"
+        "  FIFO  - path to fifo\n"
+        "  INPUT - path to input file\n"
+        "  N     - read burst size\n";
+
 int main(int argc, char **argv)
 {
-    printf("Consumer\n");
+    if (argc != 4)
+    {
+        fprintf(stderr, HELP, argv[0]);
+        return -1;
+    }
+
     return 0;
 }
