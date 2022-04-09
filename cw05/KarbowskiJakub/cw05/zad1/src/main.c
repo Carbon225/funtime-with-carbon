6095673f7466f2252b6535473a81ce9eca005231
Carbon225 <carbon225@protonmail.com>
2022-04-09 19:22:25 +0200

    [c05] print errors

--- cw05/KarbowskiJakub/cw05/zad1/src/main.c
+++ cw05/KarbowskiJakub/cw05/zad1/src/main.c
@@ -2,9 +2,18 @@
 #include "parser.h"
 #include "executor.h"
 
+static const char HELP[] =
+        "SO Lab5 Zad1 - Jakub Karbowski\n"
+        "Usage:\n"
+        "%s FILE - run commands from FILE\n";
+
 int main(int argc, char** argv)
 {
-    if (argc != 2) return -1;
+    if (argc != 2)
+    {
+        fprintf(stderr, HELP, argv[0]);
+        return -1;
+    }
 
     const char *input_file = argv[1];
 
@@ -12,7 +21,11 @@ int main(int argc, char** argv)
     parser_init(&parser);
 
     FILE *f = fopen(input_file, "r");
-    if (!f) return -1;
+    if (!f)
+    {
+        fprintf(stderr, "Could not open file\n");
+        return -1;
+    }
 
     int err = 0;
 
@@ -34,10 +47,11 @@ int main(int argc, char** argv)
     fclose(f);
 
     if (parser.state == PARSER_S_ERR)
-        fprintf(stderr, "%s\n", parser.err_msg);
+        fprintf(stderr, "Parser error:\n%s\n", parser.err_msg);
     else
     {
         err = program_execute(&parser.program);
+        if (err) fprintf(stderr, "Execution error\n");
     }
 
     return err;
