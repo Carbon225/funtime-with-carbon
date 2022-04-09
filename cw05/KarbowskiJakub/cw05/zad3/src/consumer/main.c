9a25851f32c4a47a0f0d5ac4ef6ac43bc7ea2b58
Carbon225 <carbon225@protonmail.com>
2022-04-09 20:07:18 +0200

    [c05] open files

--- cw05/KarbowskiJakub/cw05/zad3/src/consumer/main.c
+++ cw05/KarbowskiJakub/cw05/zad3/src/consumer/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 static const char HELP[] =
         "SO Lab5 Zad3 - Jakub Karbowski\n"
         "Usage:\n"
         "%s FIFO OUTPUT N\n"
-        "  FIFO  - path to fifo\n"
-        "  INPUT - path to input file\n"
-        "  N     - read burst size\n";
+        "  FIFO   - path to fifo\n"
+        "  OUTPUT - path to output file\n"
+        "  N      - read burst size\n";
 
 int main(int argc, char **argv)
 {
@@ -15,6 +18,35 @@ int main(int argc, char **argv)
         fprintf(stderr, HELP, argv[0]);
         return -1;
     }
+
+    const char *fifo_path = argv[1];
+    const char *output_file_path = argv[2];
+
+    char *endptr;
+    int burst_size = (int) strtol(argv[3], &endptr, 10);
+    if (*endptr || burst_size < 0)
+    {
+        fprintf(stderr, "Invalid N: %s\n", argv[3]);
+        return -1;
+    }
+
+    int fifo_fd = open(fifo_path, O_RDONLY);
+    if (fifo_fd < 0)
+    {
+        fprintf(stderr, "Could not open fifo\n");
+        return -1;
+    }
+
+    int output_fd = open(output_file_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (output_fd < 0)
+    {
+        fprintf(stderr, "Could not open output file\n");
+        close(fifo_fd);
+        return -1;
+    }
+
+    close(fifo_fd);
+    close(output_fd);
 
     return 0;
 }
--- cw05/KarbowskiJakub/cw05/zad3/src/producer/main.c
+++ cw05/KarbowskiJakub/cw05/zad3/src/producer/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 static const char HELP[] =
         "SO Lab5 Zad3 - Jakub Karbowski\n"
@@ -16,6 +19,42 @@ int main(int argc, char **argv)
         fprintf(stderr, HELP, argv[0]);
         return -1;
     }
+
+    const char *fifo_path = argv[1];
+    const char *input_file_path = argv[3];
+
+    char *endptr;
+    int line = (int) strtol(argv[2], &endptr, 10);
+    if (*endptr || line < 0)
+    {
+        fprintf(stderr, "Invalid LINE: %s\n", argv[2]);
+        return -1;
+    }
+
+    int burst_size = (int) strtol(argv[4], &endptr, 10);
+    if (*endptr || burst_size < 0)
+    {
+        fprintf(stderr, "Invalid N: %s\n", argv[4]);
+        return -1;
+    }
+
+    int fifo_fd = open(fifo_path, O_WRONLY);
+    if (fifo_fd < 0)
+    {
+        fprintf(stderr, "Could not open fifo\n");
+        return -1;
+    }
+
+    int input_fd = open(input_file_path, O_RDONLY);
+    if (input_fd < 0)
+    {
+        fprintf(stderr, "Could not open input file\n");
+        close(fifo_fd);
+        return -1;
+    }
+
+    close(fifo_fd);
+    close(input_fd);
 
     return 0;
 }
