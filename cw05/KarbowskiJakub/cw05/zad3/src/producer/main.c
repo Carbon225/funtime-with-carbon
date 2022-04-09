e57fbbddcaadbdbf2f7b332b65ab7ec9e91b19cb
Carbon225 <carbon225@protonmail.com>
2022-04-09 19:42:52 +0200

    [c05] producer help

--- cw05/KarbowskiJakub/cw05/zad3/src/producer/main.c
+++ cw05/KarbowskiJakub/cw05/zad3/src/producer/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+static const char HELP[] =
+        "SO Lab5 Zad3 - Jakub Karbowski\n"
+        "Usage:\n"
+        "%s FIFO LINE INPUT N\n"
+        "  FIFO  - path to fifo\n"
+        "  LINE  - line number\n"
+        "  INPUT - path to input file\n"
+        "  N     - read burst size\n";
+
 int main(int argc, char **argv)
 {
-    printf("Producer\n");
+    if (argc != 5)
+    {
+        fprintf(stderr, HELP, argv[0]);
+        return -1;
+    }
+
     return 0;
 }
