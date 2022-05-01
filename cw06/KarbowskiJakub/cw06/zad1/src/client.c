657abeed7ae8b19a2771a85e6e66695f657b6ad8
Carbon225 <carbon225@protonmail.com>
2022-05-01 16:01:10 +0200

    [c06] logs

--- cw06/KarbowskiJakub/cw06/zad1/src/client.c
+++ cw06/KarbowskiJakub/cw06/zad1/src/client.c
@@ -9,8 +9,6 @@
 
 int main(int argc, char **argv)
 {
-    printf("Client\n");
-
     client_t client;
     if (client_init(&client)) return -1;
 
@@ -26,7 +24,7 @@ int main(int argc, char **argv)
         if ((err = client_loop(&client))) break;
     } while (0);
 
-    err = err ? err : client_delete_queue(&client);
+    client_delete_queue(&client);
 
     client_free(&client);
 
@@ -35,59 +33,76 @@ int main(int argc, char **argv)
 
 int client_init(client_t *client)
 {
+    printf("[I] Creating client\n");
     client->client_queue = -1;
     client->server_queue = -1;
+    printf("[I] OK\n");
     return 0;
 }
 
 void client_free(client_t *client)
 {
-
+    printf("[I] Removing client\n");
+    printf("[I] OK\n");
 }
 
 
 int client_create_queue(client_t *client)
 {
+    printf("[I] Creating client queue\n");
     client->client_queue = msgget(IPC_PRIVATE, 0600);
     if (client->client_queue < 0)
     {
-        perror("Could not create client queue");
+        perror("[E] Could not create client queue");
         return -1;
     }
+    printf("[I] OK\n");
     return 0;
 }
 
 int client_delete_queue(client_t *client)
 {
+    printf("[I] Deleting client queue\n");
     if (msgctl(client->client_queue, IPC_RMID, NULL))
     {
-        perror("Count not remove client queue");
+        perror("[E] Count not remove client queue");
         return -1;
     }
+    printf("[I] OK\n");
     return 0;
 }
 
 
 int client_open_server(client_t *client)
 {
+    printf("[I] Opening server queue\n");
     const char *home = getenv("HOME");
     key_t key = ftok(home, SERVER_QUEUE_PROJ_ID);
     client->server_queue = msgget(key,0600);
     if (client->server_queue < 0)
     {
-        perror("Could not open server queue");
+        perror("[E] Could not open server queue");
         return -1;
     }
+    printf("[I] OK\n");
     return 0;
 }
 
 int client_send_init(client_t *client)
 {
+    printf("[I] Client sending INIT\n");
+    printf("[I] OK\n");
     return 0;
 }
 
 
 int client_loop(client_t *client)
 {
+    printf("[I] Starting client loop\n");
+    for (;;)
+    {
+        break;
+    }
+    printf("[I] Client loop done\n");
     return 0;
 }
--- cw06/KarbowskiJakub/cw06/zad1/src/server.c
+++ cw06/KarbowskiJakub/cw06/zad1/src/server.c
@@ -9,8 +9,6 @@
 
 int main(int argc, char **argv)
 {
-    printf("Server\n");
-
     server_t server;
     if (server_init(&server)) return -1;
 
@@ -22,7 +20,7 @@ int main(int argc, char **argv)
         if ((err = server_loop(&server))) break;
     } while (0);
 
-    err = err ? err : server_delete_queue(&server);
+    server_delete_queue(&server);
 
     server_free(&server);
 
@@ -31,40 +29,53 @@ int main(int argc, char **argv)
 
 int server_init(server_t *server)
 {
+    printf("[I] Creating server\n");
     server->server_queue = -1;
+    printf("[I] OK\n");
     return 0;
 }
 
 void server_free(server_t *server)
 {
-
+    printf("[I] Removing server\n");
+    printf("[I] OK\n");
 }
 
 
 int server_create_queue(server_t *server)
 {
+    printf("[I] Creating server queue\n");
     const char *home = getenv("HOME");
     key_t key = ftok(home, SERVER_QUEUE_PROJ_ID);
     server->server_queue = msgget(key,IPC_CREAT | 0600);
     if (server->server_queue < 0)
     {
-        perror("Could not create server queue");
+        perror("[E] Could not create server queue");
         return -1;
     }
+    printf("[I] OK\n");
     return 0;
 }
 
 int server_delete_queue(server_t *server)
 {
+    printf("[I] Deleting server queue\n");
     if (msgctl(server->server_queue, IPC_RMID, NULL))
     {
-        perror("Could not remove server queue");
+        perror("[E] Could not remove server queue");
         return -1;
     }
+    printf("[I] OK\n");
     return 0;
 }
 
 int server_loop(server_t *server)
 {
+    printf("[I] Starting server loop\n");
+    for (;;)
+    {
+        break;
+    }
+    printf("[I] Server loop done\n");
     return 0;
 }
